068e44c7c4eef6502274f2884fd3ec7cb25c17da
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-26 21:53:37 -0600

    palendrome and psudocode lab

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word == undefined || word == null)
+  {
+    throw "word must be defined and not null!";
+  }
+  //else if (word.length == 0) return true;
+  else
+  {
+    for(let n = 0; n < word.length/2; n++)
+    {
+      if (word.charAt(word.length - 1 - n) === word.charAt(n));
+      else return false;
+    }
+    return true;
+  }
 }
 
 /* 
   Add your pseudocode here
+  palendromes are symetric
+  meaning aba is a palendrome
+  where the first character matches the last character in the word
+  but not only that but acbca is also a palendrome
+  the first character matches the last character and
+  the second to last matches the second character and so on...
+  but is an example of something that is not
+  racecar is another palendrome of odd length (7)
+  abba is a palendrome of even length (4)
+  
+  so a shortcut would be:
+  base case length 0 is a palendrome return true
+  base case the string must be defined, and it must not be null throw error
+  divide the string length in half then get two parts
+  if odd leave middle out
+  aba has 3 characters in it
+  the middle character is 3/2=1.5 but integer rounds down (truncates) to 1
+  OPTION 1: we need to reverse the first part of the string
+  then see if it matches the second part leaving out middle character if odd length
+  if even length, both parts must equal each other for a return value of true
+  OPTION 2: for each character at length - 1 - n and at n will match
+  increment n and repeat
+  if we get to the middle return true
+  if at any time no match return false
 */
 
 /*
